a9237da963d6c671fbd34c2d06d87d75c535cf06
Eliot Eikenberry <eliote@liatrio.com>
2024-08-19 13:56:24 -0400

    fix: forgot to add this to scoreboard (#74)

--- src/ScoreBoard/ScoreBoard.tsx
+++ src/ScoreBoard/ScoreBoard.tsx
@@ -79,6 +79,14 @@ const ScoreBoard : React.FC<ChartProps> = (props: ChartProps) => {
     fetchData(props, organizeData)
   }, [props])
 
+  if (props.message) {
+    return (
+      <div data-testid="RecoverTime" style={{ width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <span style={{color: "white"}}>{props.message}</span>
+      </div>
+    )
+  }
+
   if(loading || props.loading) {
       return (
           <div data-testid="ScoreBoard" style={{width: "100%", height: "100%", paddingTop: "10px", paddingBottom: "100px"}}>
